fe4281d4e793ad77ee5678700177d2e99caaa308
Mario Andres Botero Florez <mariobot113@hotmail.com>
2016-09-30 11:29:29 -0500

    add MyTunes Project XAM110

---
+++ MyTunes/MyTunes.Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using System.Collections.Generic;
+using MyTunes.Shared;
+using System.Linq;
+using System.IO;
+
+namespace MyTunes
+{
+	[Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : ListActivity
+	{
+		protected async override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+            //ListAdapter = new ListAdapter<string>() {                
+            //	DataSource = new[] { "One", "Two", "Three" }
+            //};
+
+            var _songs = await SongLoader.Load();
+
+            ListAdapter = new ListAdapter<Song>()
+            {   
+                DataSource = _songs.ToList(),
+                TextProc = s => s.Name,
+                DetailTextProc = s => s.Artist + " - " + s.Album
+            };
+
+        }
+    }
+}
+
+
