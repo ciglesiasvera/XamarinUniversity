0ddd430c1f17eb9f201fb3ca58427fda3931f79f
Mario Andres Botero Florez <mariobot113@hotmail.com>
2016-09-30 14:40:11 -0500

    Add MyTunes2 for the seconds examples with PCL

---
+++ MyTunes2/MyTunes.Droid/StreamLoader.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+using MyTunes.Shared;
+
+namespace MyTunes
+{
+    public class StreamLoader : IStreamLoader
+    {
+        private readonly Context context;
+
+        public StreamLoader(Context context)
+        {
+            this.context = context;
+        }
+        public Stream GetStreamForFilename(string filename)
+        {
+            return context.Assets.Open(filename);
+        }
+    }
+}
