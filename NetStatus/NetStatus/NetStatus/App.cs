364dd998101118b906358611d6eb6f3deeaadc68
Mario Andres Botero Florez <mariobot113@hotmail.com>
2016-10-04 10:28:17 -0500

    Add NetStatus Project

---
+++ NetStatus/NetStatus/NetStatus/App.cs
@@ -0,0 +1,46 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+    public class App : Application
+    {
+        public App()
+        {
+            //MainPage = new NavigationPage(new NetworkStatus());
+            MainPage = CrossConnectivity.Current.IsConnected ? (Page)new NetworkViewPage() : new NoNetworkPage();
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            base.OnStart();
+            CrossConnectivity.Current.ConnectivityChanged += Current_ConnectivityChanged;
+        }
+
+        private void Current_ConnectivityChanged(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e)
+        {
+            Type currentPage = this.MainPage.GetType();
+            if (e.IsConnected && currentPage != typeof(NetworkViewPage))
+                this.MainPage = new NetworkViewPage();
+            else if (!e.IsConnected && currentPage != typeof(NoNetworkPage))
+                this.MainPage = new NoNetworkPage();
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ NetStatus/NetStatus/NetStatus/NetworkViewPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+    public partial class NetworkViewPage : ContentPage
+    {
+        public NetworkViewPage()
+        {
+            InitializeComponent();
+            //BackgroundColor = Color.FromRgb(0xf0, 0xf0, 0xf0);
+            //Content = new Label()
+            //{
+            //    Text = "Connection Available",
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.Center,
+            //    TextColor = Color.FromRgb(0x40, 0x40, 0x40),
+            //};
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ConnectionDetails.Text = CrossConnectivity.Current.ConnectionTypes.First().ToString();
+
+            if (CrossConnectivity.Current != null)
+                CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+        }
+
+        private void UpdateNetworkInfo(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e)
+        {
+            if (CrossConnectivity.Current != null && CrossConnectivity.Current.ConnectionTypes != null)
+            {
+                var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                ConnectionDetails.Text = connectionType.ToString();
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            if (CrossConnectivity.Current != null)
+                CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+        }
+        
+    }
+}
