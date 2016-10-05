1e7df5ae9db21fc5ed1120e09dc506e7395b0cdc
Mario Andres Botero Florez <mariobot113@hotmail.com>
2016-10-05 15:17:25 -0500

    add sqllite project, add more filds to clases and runs the project

---
+++ People2/People/People/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace People.Models
+{
+    [Table("people")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Email { get; set; }
+
+        [MaxLength(50),Unique]
+        public string Twitter { get; set; }
+    }
+}
